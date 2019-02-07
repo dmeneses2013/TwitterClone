cb63362679631a96127c349e2d777227a934ae4a
dmeneses2013 <dmeneses2013@gmail.com>
2019-02-08 07:35:34 +1100

    Twitter Clone initial commit

---
+++ app/assets/javascripts/components/_main.js.jsx
@@ -0,0 +1,24 @@
+const Main = (props) => {
+    return(
+      <div>
+        <h1>Twitter Clone</h1>
+        <li>The twitter app will consists of a simple login, home, messages, search, and account pages.</li>
+          <li>When new user visits site, we'll do simple login, similar to the 'sessions' rails controller done in class earlier</li>
+          <li>After sign in, user lands on Home page with stories from all users</li>
+          <li>Let's skip adding friends, we'll just show all tweets from all users</li>
+            <h3> Tasks</h3>
+            <li>Daniel - I can start on react Home, Account pages.</li>
+            <li>Arun - </li>
+            <li>Inder - </li>
+        <h3>To do list</h3>
+        <li>Rails models</li>
+        <img src ='https://cdn-images-1.medium.com/max/1600/1*f3ZrU4zgYPCsDsvtZcwSnQ.png'></img>
+        <li>Reference: https://codeburst.io/making-a-twitter-clone-in-raw-html-with-ruby-on-rails-and-a-lil-javascript-270a5e227cb2 </li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </div>
+    )
+}
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+
+  def index
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def update
+
+  end
+
+
+
+end
